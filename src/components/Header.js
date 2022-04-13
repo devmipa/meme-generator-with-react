7d3fa08c82a54b0a662b7a7ce7ce1c2d761d8e85
devmipa <devmipa@gmail.com>
2022-04-13 18:50:50 -0400

    Create header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header(){
+    return(
+        <header>
+            <img src='/Troll-Face.png'/>
+            <h2 className='header--title'>Meme Generator</h2>
+            <h4 className="header--project">React -- project</h4>
+        </header>
+    )
+}
